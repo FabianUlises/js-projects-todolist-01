184ba636dabc11c07f03e12ecd41bc1c397b1114
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-11-02 09:08:37 -0700

    Added message if no input is invalid

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 // Selectors
 const todoBtn = document.querySelector('#todo-btn');
-const todoInput = document.querySelector('#task-ame');
+const todoInput = document.querySelector('#task-name');
 const todoCtn = document.querySelector('.todo-list');
 // Function to add task
 const addTodo = (e) => {
+    if(!todoInput.value){
+        todoInput.placeholder = 'enter a task !'
+        return;
+    }
     // Prevent from submitting
     e.preventDefault();
-    // Test console log
-    console.log('button pressed');
     // Creating todo div
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
     // Creating todo li
     const todoItem = document.createElement('li');
     todoItem.classList.add('todo-item');
-    todoItem.innerText = 'push ups';
+    todoItem.innerText = todoInput.value;
     todoDiv.append(todoItem);
     todoCtn.append(todoDiv);
+    todoInput.value = '';
 };
+
 todoBtn.addEventListener('click', addTodo)
