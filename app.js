6eff5da5a7caaad99bbc876ff6696c2856833015
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-11-02 11:03:01 -0700

    Added completed class. and function to remove todo item

--- app.js
+++ app.js
@@ -18,8 +18,35 @@ const addTodo = (e) => {
     todoItem.classList.add('todo-item');
     todoItem.innerText = todoInput.value;
     todoDiv.append(todoItem);
+    // Check button
+    const completedBtn = document.createElement('button');
+    completedBtn.innerHTML = `<i class='fa-solid fa-check'></i>`
+    completedBtn.classList.add('complete-btn');
+    // Trash button
+    const trashBtn = document.createElement('button');
+    trashBtn.innerHTML = `<i class="fa-solid fa-trash">t</i>`
+    trashBtn.classList.add('trash-btn');
+    todoDiv.append(trashBtn);
+    todoDiv.append(completedBtn);
     todoCtn.append(todoDiv);
     todoInput.value = '';
 };
+// Function to remove task
+const deleteTodo = (e) => {
+    const item = e.target;
+    if(item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', () => {
+            todo.remove();
+        });
 
-todoBtn.addEventListener('click', addTodo)
+    };
+    if(item.classList[0] === 'complete-btn') {
+        // console.log('button pressed')
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    };
+};
+todoBtn.addEventListener('click', addTodo);
+todoCtn.addEventListener('click', deleteTodo);
