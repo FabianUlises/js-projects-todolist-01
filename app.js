873c78ed73eacd097694940c75f5988584c55304
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-11-02 11:25:09 -0700

    Cleaning up code & adding comments

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const todoInput = document.querySelector('#task-name');
 const todoCtn = document.querySelector('.todo-list');
 // Function to add task
 const addTodo = (e) => {
+    // If no input
     if(!todoInput.value){
+        // Change placeholder text to 'enter a task!'
         todoInput.placeholder = 'enter a task !'
         return;
-    }
+    };
     // Prevent from submitting
-    e.preventDefault();
+    // e.preventDefault();
     // Creating todo div
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
@@ -24,29 +26,40 @@ const addTodo = (e) => {
     completedBtn.classList.add('complete-btn');
     // Trash button
     const trashBtn = document.createElement('button');
-    trashBtn.innerHTML = `<i class="fa-solid fa-trash">t</i>`
+    trashBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`
     trashBtn.classList.add('trash-btn');
+    // Adding items to html
     todoDiv.append(trashBtn);
     todoDiv.append(completedBtn);
     todoCtn.append(todoDiv);
     todoInput.value = '';
 };
+
 // Function to remove task
 const deleteTodo = (e) => {
+    // Setting item to equal element that was clicked
     const item = e.target;
+    // If item contains classlist 'trash-btn'
     if(item.classList[0] === 'trash-btn') {
+        // Selecting parent item from button (todo div) to remove
         const todo = item.parentElement;
+        // Adding animation class
         todo.classList.add('fall');
+        // Event listener to wait for transition to end
         todo.addEventListener('transitionend', () => {
+            // Remove todo after transition ends
             todo.remove();
         });
-
     };
+    // If item contains classlist 'complete-btn'
     if(item.classList[0] === 'complete-btn') {
         // console.log('button pressed')
+        // Selecting parent item from button (todo div) to add class showing completed
         const todo = item.parentElement;
+        // Adding completed class
         todo.classList.toggle('completed');
     };
 };
+// Eventlisteners
 todoBtn.addEventListener('click', addTodo);
 todoCtn.addEventListener('click', deleteTodo);
